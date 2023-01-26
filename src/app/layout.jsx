0c0d41009e4e26cf644c37c96ad910edb2103c09
Mayra <mayra203@gmail.com>
2023-01-26 13:25:18 -0600

    First commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,11 +3,9 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+      <head>
+        <title>Portfolio</title>
+      </head>
       <body>{children}</body>
     </html>
   )
