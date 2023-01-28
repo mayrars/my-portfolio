9d148ca09fb1706163eb505c672c69f53ab461b9
Mayra <mayra203@gmail.com>
2023-01-27 18:00:46 -0600

    Added text in the loading file

--- src/app/projects/[id]/loading.jsx
+++ src/app/projects/[id]/loading.jsx
@@ -6,8 +6,9 @@ export default function Loading () {
   return (
     <>
       <Card style={{ width: '25%' }} className={styles.card}>
-        <Placeholder className={styles.image} lg={12} md={12} sm={12} />
-        <Placeholder xs={3} lg={12} />
+        <Placeholder className={styles.image} lg={12} md={12} sm={12} bg='primary' />
+        <Placeholder xs={3} lg={12} bg='secondary' />
+        <Placeholder xs={3} lg={6} bg='secondary' />
       </Card>
     </>
   )
