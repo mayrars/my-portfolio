ea9067c98b83be6d947d2e1998adee7c455a4fff
Mayra <mayra203@gmail.com>
2023-01-31 10:17:56 -0600

    Deleted extra space

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,6 @@ import Headerbar from './components/Headerbar'
 import './globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-
 export default function RootLayout ({ children }) {
   return (
     <html lang='en'>
