189abe5a18e0f19be54561c29f53934851a1a604
Mayra <mayra203@gmail.com>
2023-01-31 23:45:39 -0600

    Changed padding top

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,15 +1,15 @@
-'use client'
-import { Col, Container, Row } from 'react-bootstrap'
+"use client";
+import { Col, Container, Row } from "react-bootstrap";
 
-export default function Projects () {
-  return (
-    <>
-      <Container fluid='md'>
-        <h1>Projects</h1>
-        <Row>
-          <Col>1</Col>
-        </Row>
-      </Container>
-    </>
-  )
+export default function Projects() {
+    return (
+        <>
+            <Container className="pt-4">
+                <h1>Projects</h1>
+                <Row>
+                    <Col>1</Col>
+                </Row>
+            </Container>
+        </>
+    );
 }
