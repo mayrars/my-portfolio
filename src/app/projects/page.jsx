a86b136192341d377f35c74b1ae8f951392e41df
Mayra <mayra203@gmail.com>
2023-02-13 17:24:07 -0600

    Asjusting with new API projects

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,15 +1,14 @@
-"use client";
-import { Col, Container, Row } from "react-bootstrap";
+'use client'
+import { Container } from 'react-bootstrap'
 
-export default function Projects() {
-    return (
-        <>
-            <Container className="pt-4">
-                <h1>Projects</h1>
-                <Row>
-                    <Col>1</Col>
-                </Row>
-            </Container>
-        </>
-    );
+export default async function Projectslist (projects) {
+  const req = await fetch('api/projects')
+  const res = req.json()
+  return (
+    <>
+      <Container className='pt-4'>
+        <h1>Projects</h1>
+      </Container>
+    </>
+  )
 }
